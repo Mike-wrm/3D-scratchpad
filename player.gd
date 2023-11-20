08e812c7ca040601983935e534572d0f9a0185df
Michael <spikeymikey96@live.com>
2023-11-20 12:52:10 -0600

    Player movement added
    Added the ability to move the player via WARS keys (colemak). Additionally added faux gravity (constant downward velocity, not acceleration).

--- player.gd
+++ player.gd
@@ -1,8 +1,33 @@
 extends CharacterBody3D
 
+@export var speed:float = 20
+
+const GRAVITY:float = 20
+
 var look_target:Vector3
 
-func _physics_process(delta):
+func _process(delta):
 	self.look_at(look_target, Vector3.UP)
-	pass
 	
+	
+func _physics_process(delta):
+	var move_vector := Vector3.ZERO
+	
+	if Input.is_action_pressed("move_up"):
+		move_vector.z -= 1
+	if Input.is_action_pressed("move_down"):
+		move_vector.z += 1
+	if Input.is_action_pressed("move_left"):
+		move_vector.x -= 1
+	if Input.is_action_pressed("move_right"):
+		move_vector.x += 1
+	
+	if move_vector.length() > 1:
+		move_vector.normalized()
+	move_vector *= speed
+		
+	if not is_on_floor():
+		move_vector.y -= GRAVITY
+		
+	velocity = move_vector 
+	move_and_slide()
